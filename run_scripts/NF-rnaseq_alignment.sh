4cb49989055e548e7fa36ccec18cadb996f27bd0
Alex Thiery <alexthiery1@gmail.com>
2023-01-20 15:08:56 +0000

    init rnaseq alignment from nfcore test data

---
+++ run_scripts/NF-rnaseq_alignment.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --job-name=nf-core_rnaseq_alignment
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=alex.thiery@crick.ac.uk
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/21.10.6
+ml Singularity/3.6.4
+ml Graphviz
+
+export TERM=xterm
+export NXF_VER=21.10.6
+export NXF_SINGULARITY_CACHEDIR=/camp/home/thierya/working/NF_singularity
+
+## UPDATE PIPLINE
+nextflow pull nf-core/rnaseq
+
+## RUN alignment
+nextflow run nf-core/rnaseq \
+    -profile test_full \
+    -c ./conf/crick.config \
+    --outdir ./output/NF-RNAseq_alignment \
+    --email alex.thiery@crick.ac.uk \
+    -resume
